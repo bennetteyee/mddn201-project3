f370ed12ddfa09c408cdd62f33d7511083879a24
Bennette Yee <yeebenn@ecs.vuw.ac.nz>
2019-09-25 11:51:33 +1200

    Fixed functionality bugs

--- logic.js
+++ logic.js
@@ -1,6 +1,5 @@
 function toggleSidebar() {
    document.getElementById("sidebar").classList.toggle('active');
-    console.log('works');
 }
 
 function changeImage1() {
@@ -8,11 +7,52 @@ function changeImage1() {
     var image = document.getElementById("image1");
     
     if(imageTracker === 'img'){
-        image.src ='img/img-1.jpg';
+        image.src ='img/img1-blur.jpg';
         imageTracker = 'img-blur';
-    }else{
+    }else if(imageTracker === 'img-blur'){
         image.src='img/img1.jpg';
-         imageTracker = 'img';
+        imageTracker = 'img';
+    }
+       
+}
+
+function changeImage2() {
+    var imageTracker = 'img';
+    var image = document.getElementById("image2");
+    
+    if(imageTracker === 'img'){
+        image.src ='img/img2.jpg';
+    }else if(imageTracker === 'img-blur'){
+        image.src='img/img1.jpg';
+        imageTracker = 'img';
+    }
+       
+}
+
+function changeImage3() {
+    var imageTracker = 'img';
+    var image = document.getElementById("image3");
+    
+    if(imageTracker === 'img'){
+        image.src ='img/img3-blur.jpg';
+        imageTracker = 'img-blur';
+    }else if(imageTracker === 'img-blur'){
+        image.src='img/img3.jpg';
+        imageTracker = 'img';
+    }
+       
+}
+
+function changeImage4() {
+    var imageTracker = 'img';
+    var image = document.getElementById("image4");
+    
+    if(imageTracker === 'img'){
+        image.src ='img/img4.jpg';
+        imageTracker = 'img-blur';
+    }else if(imageTracker === 'img-blur'){
+        image.src='img/img1.jpg';
+        imageTracker = 'img';
     }
        
 }
